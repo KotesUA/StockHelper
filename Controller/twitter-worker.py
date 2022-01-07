1a838ac9105e201e9208164fc7e4be5d93b467fa
Kotes <51745529+KotesUA@users.noreply.github.com>
2022-01-07 16:25:20 +0200

    Done with twitter mood

--- Controller/twitter-worker.py
+++ Controller/twitter-worker.py
@@ -2,76 +2,42 @@ import requests
 import json
 from keys import TWITTER_BEARER_TOKEN
 
-URL = "https://api.twitter.com/2/tweets/search/stream/rules"
-
 
 def bearer_oauth(r):
-    r.HEADERS["Authorization"] = f"Bearer {TWITTER_BEARER_TOKEN}"
-    r.HEADERS["User-Agent"] = "StockHelperKotes"
+    r.headers["Authorization"] = f"Bearer {TWITTER_BEARER_TOKEN}"
+    r.headers["User-Agent"] = "StockHelperKotes"
     return r
 
 
-def get_rules():
-    response = requests.get(URL, auth=bearer_oauth)
+def search_tweets(ticker):
+    url = "https://api.twitter.com/2/tweets/search/recent"
+    query_params = {
+        'query': f'({ticker} -rich -free -miss -project) lang:en (bullish OR bearish)',
+        'tweet.fields': 'author_id',
+        'max_results': '100'
+    }
+    response = requests.request("GET", url, auth=bearer_oauth, params=query_params)
     if response.status_code != 200:
-        raise Exception(
-            "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
-        )
-    print(json.dumps(response.json()))
-    return response.json()
+        print("Request returned an error: {} {}".format(response.status_code, response.text))
+    else:
+        return response.json()
 
 
-def delete_all_rules(rules):
-    if rules is None or "data" not in rules:
-        return None
-
-    ids = list(map(lambda rule: rule["id"], rules["data"]))
-    payload = {"delete": {"ids": ids}}
-    response = requests.post(URL, auth=bearer_oauth, json=payload)
-    if response.status_code != 200:
-        raise Exception(
-            "Cannot delete rules (HTTP {}): {}".format(
-                response.status_code, response.text
-            )
-        )
-    print(json.dumps(response.json()))
-
-
-def set_rules(delete):
-    sample_rules = [
-        {"value": "", "tag": "dog pictures"},
-        {"value": "cat has:images -grumpy", "tag": "cat pictures"},
-    ]
-    payload = {"add": sample_rules}
-    response = requests.post(URL, auth=bearer_oauth, json=payload)
-    if response.status_code != 201:
-        raise Exception(
-            "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
-        )
-    print(json.dumps(response.json()))
-
-
-def get_stream(set):
-    response = requests.get(URL, auth=bearer_oauth, stream=True)
-    print(response.status_code)
-    if response.status_code != 200:
-        raise Exception(
-            "Cannot get stream (HTTP {}): {}".format(
-                response.status_code, response.text
-            )
-        )
-    for response_line in response.iter_lines():
-        if response_line:
-            json_response = json.loads(response_line)
-            print(json.dumps(json_response, indent=4, sort_keys=True))
-
-
-def main():
-    rules = get_rules()
-    delete = delete_all_rules(rules)
-    set = set_rules(delete)
-    get_stream(set)
+def mood(ticker):
+    feed = search_tweets(ticker)
+    bulls = 0
+    bears = 0
+    for tweet in feed['data']:
+        if "bull" in tweet["text"]:
+            bears += 1
+        else:
+            if "bear" in tweet["text"]:
+                bulls += 1
+    return bulls, bears
 
 
 if __name__ == "__main__":
-    main()
+    ticker = 'BTC'
+    print(json.dumps(search_tweets(ticker), indent=4, sort_keys=True))
+    bulls, bears = mood(ticker)
+    print(f'Twitter mood for {ticker}:\n\tBulls: {bulls}\n\tBears: {bears}')

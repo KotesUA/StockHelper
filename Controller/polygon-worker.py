83aefef69c82029a40d268adca766a70107e9700
Kotes <51745529+KotesUA@users.noreply.github.com>
2022-01-07 14:57:50 +0200

    Added polygon.io API worker

--- Controller/polygon-worker.py
+++ Controller/polygon-worker.py
@@ -1,28 +1,20 @@
-import datetime
+from datetime import date, timedelta
 
-from polygon import RESTClient
+import requests
+from keys import POLYGON_KEY
 
-
-def ts_to_datetime(ts) -> str:
-    return datetime.datetime.fromtimestamp(ts / 1000.0).strftime('%Y-%m-%d %H:%M')
-
-
-def main():
-    key = "your api key"
-
-    # RESTClient can be used as a context manager to facilitate closing the underlying http session
-    # https://requests.readthedocs.io/en/master/user/advanced/#session-objects
-    with RESTClient(key) as client:
-        from_ = "2019-01-01"
-        to = "2019-02-01"
-        resp = client.stocks_equities_aggregates("AAPL", 1, "minute", from_, to, unadjusted=False)
-
-        print(f"Minute aggregates for {resp.ticker} between {from_} and {to}.")
-
-        for result in resp.results:
-            dt = ts_to_datetime(result["t"])
-            print(f"{dt}\n\tO: {result['o']}\n\tH: {result['h']}\n\tL: {result['l']}\n\tC: {result['c']} ")
+URL = "https://api.polygon.io/v1/open-close/"
 
 
-if __name__ == '__main__':
-    main()
+def get_news(ticker):
+    querystring = f"{URL}{ticker}/{date.today() - timedelta(days=1)}?apiKey={POLYGON_KEY}"
+    print(querystring)
+    response = requests.request("GET", querystring)
+    if response.status_code != 200:
+        print("Cannot get data (HTTP {}): {}".format(response.status_code, response.text))
+    else:
+        print(response.text)
+
+
+if __name__ == "__main__":
+    get_news("AAPL")

07747a69f856000fec5d8b3725e9f14de80a2270
Kotes <51745529+KotesUA@users.noreply.github.com>
2022-01-14 02:29:07 +0200

    Update twitter_worker.py

--- Server/twitter_worker.py
+++ Server/twitter_worker.py
@@ -1,11 +1,71 @@
 import requests
 from keys import TWITTER_BEARER_TOKEN
 
+import requests
+import os
+import json
+
 
 def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {TWITTER_BEARER_TOKEN}"
     r.headers["User-Agent"] = "StockHelperKotes"
     return r
+
+
+def get_rules():
+    response = requests.get("https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    print(json.dumps(response.json()))
+    return response.json()
+
+
+def delete_all_rules(rules):
+    if rules is None or "data" not in rules:
+        return None
+
+    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    payload = {"delete": {"ids": ids}}
+    response = requests.post("https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth, json=payload)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    print(json.dumps(response.json()))
+
+
+def set_rules(delete):
+    sample_rules = [
+        {"value": "dog has:images", "tag": "dog pictures"},
+        {"value": "cat has:images -grumpy", "tag": "cat pictures"},
+    ]
+    payload = {"add": sample_rules}
+    response = requests.post("https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth, json=payload)
+    if response.status_code != 201:
+        raise Exception(response.status_code, response.text)
+    print(json.dumps(response.json()))
+
+
+def get_stream(set):
+    response = requests.get("https://api.twitter.com/2/tweets/search/stream", auth=bearer_oauth, stream=True)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    for response_line in response.iter_lines():
+        if response_line:
+            json_response = json.loads(response_line)
+            print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+def main():
+    rules = get_rules()
+    delete = delete_all_rules(rules)
+    set = set_rules(delete)
+    get_stream(set)
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 def search_tweets(ticker):
